2b8b395aebc86c649d7b4692aeba521664917602
leejiwon <365support@naver.com>
2022-10-30 03:41:32 +0900

    Fix: ads 수정

--- src/component/Advertisement.jsx
+++ src/component/Advertisement.jsx
@@ -4,10 +4,11 @@ import { customBodyStyle } from "../shared/globalStyle";
 
 function Advertisement() {
   return (
-    <a href="https://www.wanted.co.kr">
-      <div css={imgWrapper} alt="ads">
+    <a href="https://www.wanted.co.kr" rel="wanted">
+      <div css={imgWrapper}>
         <img
           css={imgCss}
+          alt="ads"
           src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100"
         />
       </div>
