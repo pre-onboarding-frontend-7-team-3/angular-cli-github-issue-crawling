24e15bfd872ac85f71e68d87e9ee82e4c302c4a0
Sangmin <23mean@naver.com>
2022-10-30 21:03:19 +0900

    Fix: Syntax highlighter 추가

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -3,27 +3,22 @@ import { css } from "@emotion/react";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { octokitDetailApi } from "../api/client";
+import Reactmarkdown from "react-markdown";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import rehypeRaw from "rehype-raw";
 import { customBodyStyle } from "../shared/globalStyle";
-import Header from "../component/Header";
 import List from "../component/List";
-import markdownToHtml from "../utils/markdownParse";
 
 function Detail() {
   const navigate = useNavigate();
   const { id } = useParams();
 
-  const [issueHtml, setIssueHtml] = useState();
   const [data, setData] = useState();
 
   useEffect(() => {
     octokitDetailApi(id)
       .then((res) => {
         setData(res.data);
-        const getData = async (data) => {
-          const parseData = await markdownToHtml(data.body);
-          setIssueHtml(parseData);
-        };
-        getData(res.data);
       })
       .catch(() => {
         navigate("/error", { state: "데이터를 불러오는데 실패했습니다" });
@@ -32,19 +27,38 @@ function Detail() {
 
   return (
     <>
-      <Header />
       {data && (
-        <div css={userDataWrapper}>
-          <img css={imgCss} alt="user" src={data.user.avatar_url} />
-          <List list={data} />
-        </div>
-      )}
-      {issueHtml && (
-        <div css={customBodyStyle} dangerouslySetInnerHTML={{ __html: issueHtml }}></div>
+        <>
+          <div css={userDataWrapper}>
+            <img css={imgCss} alt="user" src={data.user.avatar_url} />
+            <List list={data} />
+          </div>
+          <Reactmarkdown
+            children={data?.body}
+            skipHtml={false}
+            // rehypePlugins={[rehypeRaw]}
+            parserOptions={{ commonmark: true }}
+            components={{ code: Component }}
+          />
+        </>
       )}
     </>
   );
 }
+
+const Component = ({ children }) => {
+  const customStyle = {
+    padding: "10px 15px",
+    margin: "22px 0",
+    borderRadius: "10px",
+    backgroundColor: "rgb(244, 244, 244)",
+  };
+  return (
+    <SyntaxHighlighter language="javascript" customStyle={customStyle}>
+      {children}
+    </SyntaxHighlighter>
+  );
+};
 
 const userDataWrapper = css`
   ${customBodyStyle}
