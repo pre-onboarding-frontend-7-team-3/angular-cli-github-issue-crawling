ddcfc58cb5c78828b6c7b4587db14012b8e8a25c
Sangmin <23mean@naver.com>
2022-11-02 12:19:56 +0900

    Fix: Route구조관련 일부 수정

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,29 +1,25 @@
 import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 
-import IssuesContextWrapper from "store/IssuesContext";
-
-import Loading from "components/common/Loading";
+import Loading from "components/common/IssueItemSkeleton";
 
 import IssueListPage from "pages/IssueList";
 const IssueDetail = lazy(() => import("pages/IssueDetail"));
 
 const RouteComponent = () => {
   return (
-    <IssuesContextWrapper>
-      <Routes>
-        <Route path="/" element={<IssueListPage />} />
+    <Routes>
+      <Route path="/" element={<IssueListPage />} />
 
-        <Route
-          path="/:id"
-          element={
-            <Suspense fallback={<Loading />}>
-              <IssueDetail />
-            </Suspense>
-          }
-        />
-      </Routes>
-    </IssuesContextWrapper>
+      <Route
+        path="/:id"
+        element={
+          <Suspense fallback={<Loading />}>
+            <IssueDetail />
+          </Suspense>
+        }
+      />
+    </Routes>
   );
 };
 
