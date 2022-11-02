8e2732be506001d8780d4d56b60490656a03c9fa
Sangmin <23mean@naver.com>
2022-11-02 12:21:02 +0900

    Fix: Context내 INIT case제거 및 useContext 선처리로 관심사 분리

--- src/store/IssuesContext.js
+++ src/store/IssuesContext.js
@@ -1,27 +1,45 @@
-import { createContext, useReducer } from "react";
+import { createContext, useContext, useMemo, useReducer } from "react";
 
-export const dispatchContext = createContext("");
 export const issuesContext = createContext("");
+export const dispatchContext = createContext("");
+
+// ! 관심사의 분리 ( 본 페이지에서의 추상화 일치성에 유리 )
+export function useStateContext() {
+  const state = useContext(issuesContext);
+  if (!state) {
+    throw new Error("useStateContext must be used withing a StateContextProvider");
+  }
+  return state;
+}
+export function useDispatchContext() {
+  const dispatch = useContext(dispatchContext);
+  if (!dispatch) {
+    throw new Error("useDispatchContext must be used withing a DispatchContextProvider");
+  }
+  return dispatch;
+}
 
 const issuesList = [];
 
 const issueReducer = (state, action) => {
   switch (action.type) {
-    case "INIT":
-      return [...action.initIssue];
     case "ADD":
-      return [...state, ...action.initIssue];
+      return [...state, ...action.addIssue];
+    case "END":
+      return action.isEnd;
     default:
       return state;
   }
 };
 
-export default function IssuesContextWrapper(props) {
+export default function IssuesContextWrapper({ children }) {
   const [state, dispatch] = useReducer(issueReducer, issuesList);
+  // !
+  const values = useMemo(() => state, [state]);
 
   return (
-    <issuesContext.Provider value={state}>
-      <dispatchContext.Provider value={dispatch}>{props.children}</dispatchContext.Provider>
+    <issuesContext.Provider value={values}>
+      <dispatchContext.Provider value={dispatch}>{children}</dispatchContext.Provider>
     </issuesContext.Provider>
   );
 }
