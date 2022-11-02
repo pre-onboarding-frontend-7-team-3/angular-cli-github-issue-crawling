fd1fd81fddf0a8080d02b2b78714312096dd59c6
Sangmin <23mean@naver.com>
2022-11-02 12:21:16 +0900

    Fix: IssueList 간소화

--- src/pages/IssueList.jsx
+++ src/pages/IssueList.jsx
@@ -1,85 +1,47 @@
-import React, { useState, useEffect, useRef, useContext } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components";
 
 import { getIssueInfo } from "api/client";
-import { dispatchContext, issuesContext } from "store/IssuesContext";
+
+import useDidMountEffect from "hooks/useDidMountEffect";
+import useInfinityScroll from "hooks/useInfinityScroll";
+
+import { useStateContext, useDispatchContext } from "store/IssuesContext";
 
 import IssueItem from "components/IssueItem";
+import AdvertisingBanner from "components/common/AdvertisingBanner";
 
-const Wrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-
-  border: 1px solid black;
-
-  a:link,
-  a:visited,
-  a:hover {
-    color: black;
-    text-decoration: none;
-  }
-`;
-
-const Img = styled.img`
-  width: 200px;
-  height: 100%;
-`;
+import { Wrapper } from "styles/PageStyle";
+import IssueListSkeleton from "components/common/IssueListSkeleton";
 
 const IssueListPage = () => {
-  const issuesData = useContext(issuesContext);
-  const dispatch = useContext(dispatchContext);
+  const issuesData = useStateContext();
+  const dispatch = useDispatchContext();
 
-  const inView = useRef();
+  const [page, setPage] = useState(Math.ceil(issuesData.length / 20));
+  const { inView } = useInfinityScroll(setPage);
 
-  const [loading, setLoading] = useState(false);
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    dispatch({ type: "INIT", initIssue: "" });
-  }, []);
-
-  useEffect(() => {
-    const getData = (page) => {
-      getIssueInfo(page).then((res) => {
-        dispatch({ type: "ADD", initIssue: res.data });
-        setLoading(true);
-      });
-    };
-
-    getData(page);
+  // !
+  useDidMountEffect(() => {
+    getIssueInfo(page).then((res) => {
+      if (res.data.length !== 0) {
+        dispatch({ type: "ADD", addIssue: res.data });
+      }
+    });
   }, [page]);
-
-  useEffect(() => {
-    if (loading) {
-      const target = new IntersectionObserver(
-        (entries) => {
-          if (entries[0].isIntersecting) {
-            setPage((page) => page + 1);
-          }
-        },
-        { threshold: 1 },
-      );
-      target.observe(inView.current);
-    }
-  }, [loading]);
 
   return (
     <Wrapper>
-      {issuesData?.map((ItemInfo, index) => (
+      {issuesData.map((ItemInfo, index) => (
         <div key={ItemInfo.id}>
-          {index === 4 && (
-            <a target="_blank" href="https://www.wanted.co.kr/" rel="noreferrer">
-              <Img src="https://image.wanted.co.kr/optimize?src=https%3A%2F%2Fstatic.wanted.co.kr%2Fimages%2Fuserweb%2Flogo_wanted_black.png&w=110&q=100" />
-            </a>
-          )}
+          {index === 4 && <AdvertisingBanner />}
           <Link to={`/${ItemInfo.number}`}>
             <IssueItem ItemInfo={ItemInfo} />
           </Link>
         </div>
       ))}
 
-      <div ref={inView} />
+      <div ref={inView}>{IssueListSkeleton()}</div>
     </Wrapper>
   );
 };
